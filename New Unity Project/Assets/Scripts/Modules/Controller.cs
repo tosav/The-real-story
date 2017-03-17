a5b107b0de02fa474ace75d6baafac80dff63515
Саргылаана <hovalanshi@gmail.com>
2017-03-17 11:44:15 +1000

    Комета, взрывы

--- New Unity Project/Assets/Scripts/Modules/Controller.cs
+++ New Unity Project/Assets/Scripts/Modules/Controller.cs
@@ -9,7 +9,10 @@ public class Controller : MonoBehaviour {
     public GameObject Bul;
     private GameObject[] Build;
     public GameObject enemy;
-    private int i = 0;
+	private int i = 0;
+	public float delayTimer= 2f;
+	float timer;
+
     private void Start()
     {
         Build = new GameObject[Building.GetComponent<Building>().buildings.Length];//количество игровых обьектов на поле
@@ -17,32 +20,38 @@ public class Controller : MonoBehaviour {
         Build[0].GetComponent<Building>().c=this;
         Build[0].GetComponent<Building>().i = 0;
         Bul.GetComponent<Building>().buildings = Building.GetComponent<Building>().buildings;
+		timer = delayTimer;
+		enemy.GetComponent<Enemy_event>().repeat= Build[0].GetComponent<Building>().repeat;
     }
 
     public void State()
     {
-        if (i + 1 < Building.GetComponent<Building>().buildings.Length)
+        if (i < Building.GetComponent<Building>().buildings.Length)
         {
             Build[i + 1] = Instantiate(Bul);
             Build[i + 1].GetComponent<Building>().i = i + 1;
             Build[i + 1].GetComponent<Building>().c = this;
             Build[i + 1].GetComponent<Building>().nextlevel = Build[0].GetComponent<Building>().nextlevel;
             Build[i + 1].GetComponent<Building>().repeat = Build[0].GetComponent<Building>().repeat;
-            Build[i + 1].GetComponent<Building>().Boom = Build[0].GetComponent<Building>().Boom;
-            i += 1;
+            Build[i + 1].GetComponent<Building>().boom = Build[0].GetComponent<Building>().boom;
+            i ++;
         }
-        else if (i + 1 == Building.GetComponent<Building>().buildings.Length)
+        else if (i == Building.GetComponent<Building>().buildings.Length)
         {
-            Building.GetComponent<Building>().repeat.GetComponent<ScrollMenu>().speed = 10;
-            Building.GetComponent<Building>().nextlevel.GetComponent<ScrollMenu>().speed = 10;
+            Building.GetComponent<Building>().repeat.GetComponent<ScrollMenu>().speed = 10f;
+            Building.GetComponent<Building>().nextlevel.GetComponent<ScrollMenu>().speed = 10f;
         }
     }
     private void OnMouseDown()
     {
         Build[i].GetComponent<Rigidbody2D>().gravityScale = 9.8f;
     }
-    public void DestroyEnemy()
-    {
-        Destroy(enemy);
-    }
+	private void Update()//тут будут рождаться враги
+	{
+		timer -= Time.deltaTime;
+		if (timer <= 0 & Building.GetComponent<Building>().repeat.GetComponent<ScrollMenu>().speed != 10f) {
+			timer= delayTimer;
+			GameObject en = Instantiate (enemy);
+		}
+	}
 }
