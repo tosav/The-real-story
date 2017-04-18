66dd8d7e972c5b1754968062a852f941627b6595
Саргылаана <hovalanshi@gmail.com>
2017-04-18 22:09:53 +1000

    Реклама убирается

--- New Unity Project/Assets/metagame/scripts/backmenu.cs
+++ New Unity Project/Assets/metagame/scripts/backmenu.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class backmenu : MonoBehaviour {
+    private void Awake()
+    {
+        Controller c = GameObject.FindGameObjectWithTag("Controller").GetComponent<Controller>();
+        MobileAdsScript scr = c.GetComponent<MobileAdsScript>();
+        scr.DestroyBannerRect();
+    }
 	void OnMouseUp()
 	{
 	SceneManager.LoadScene("main");
